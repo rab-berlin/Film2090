5292ea59cbda5f5575d472afef39f02f73f34079
rab-berlin <165693075+rab-berlin@users.noreply.github.com>
2026-05-15 16:49:16 +0200

    Program

---
+++ program/film_color.py
@@ -0,0 +1,15 @@
+import uasyncio as asyncio
+
+colors = [
+    "tuerkis","tuerkis","tuerkis","tuerkis","orange","gelb","gelb","orange",
+    "tuerkis","tuerkis","tuerkis","tuerkis","orange","gelb","gelb","orange",
+    "tuerkis","tuerkis","tuerkis","tuerkis","tuerkis","orange","orange","tuerkis",
+    "rot","rot","rot","rot","blau","blau","blau","blau",
+    "rot","rot","rot","rot","blau","blau","blau","blau",
+    "weiss","weiss","weiss","weiss","blau","blau","blau","blau",
+    "weiss","weiss","weiss","weiss","blau","blau","blau","blau",
+    "weiss","weiss","weiss","weiss","blau","blau","blau","blau"
+]
+
+setColors(colors)
+setColors(colors)
